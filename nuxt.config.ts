6837c260e43b24b5f7be81f8d1a6ac548add91d1
lschafer <lschafer@ethz.ch>
2025-09-08 15:34:25 +0200

    config update

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  modules: ['nuxt-cron'],
+  devtools: { enabled: true },
+  cron: {
+    runOnInit: true,
+    timeZone: 'Europe/Zurich',
+    jobsDir: 'cron'
+  },
+    runtimeConfig: {
+        whois: {
+        domains: ['pff26.ch', 'pff27.ch', 'pff28.ch', 'pff29.ch', 'pff30.ch', 'pff31.ch', 'pff32.ch', 'pff33.ch', 'pff34.ch', 'pff35.ch', 'pff36.ch',],
+        outputFile: 'public/whois.json'
+        },
+        public: {
+            gitSha: process.env.GIT_COMMIT_SHA || '',
+            repoUrl: process.env.REPO_URL || 'https://github.com/Sprudeel/nextPFF',
+            authorName: process.env.AUTHOR_NAME || 'Sprudel'
+        }
+    }
+})
